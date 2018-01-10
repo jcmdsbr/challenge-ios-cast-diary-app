2deb27cf1b98fa7bcd4c7343b48f08e1d54217a4
Cast Group <castgroup@castgroup-3.local>
2018-01-10 16:40:23 -0200

    ajustes no layout e comportamento de tela, evento de detalhar criado

--- Prova IOs/Sources/Controllers/AgendaViewController/AgendaTableViewController.m
+++ Prova IOs/Sources/Controllers/AgendaViewController/AgendaTableViewController.m
@@ -23,10 +23,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
+    
     NSString *nome = [[UserDefaultRepository sharedInstance] nomeUsuario];
     self.title = [NSString stringWithFormat:@"Contatos de %@",nome];
-
+    
 }
 
 - (void)viewWillAppear:(BOOL)animated{
@@ -97,8 +97,31 @@
 }
 
 
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    Contato *contato = [self.dicContatos objectForKey: @(indexPath.section)][indexPath.row];
+    
+    if(contato.enderecoCompleto.length > 0 )
+        [self criarAlertaDetalheEndereco:contato];
+    
+}
+
 #pragma marks - Ações Linha
 
+-(void) criarAlertaDetalheEndereco : (Contato*) contato {
+    
+    NSString *mensagem =  [NSString stringWithFormat:@"%@" ,contato.enderecoCompleto];
+    
+    UIAlertController *alert = [ UIAlertController  alertControllerWithTitle:@"Endereço:" message: mensagem preferredStyle:UIAlertControllerStyleAlert];
+    
+    
+    UIAlertAction *acaoOK = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil];
+    
+    [alert addAction:acaoOK];
+    
+    [self presentViewController: alert animated:YES completion:nil];
+    
+}
 -(void) editarLinha : (NSIndexPath *)indexPath {
     
     Contato *contato = [self.dicContatos objectForKey: @(indexPath.section)][indexPath.row];
